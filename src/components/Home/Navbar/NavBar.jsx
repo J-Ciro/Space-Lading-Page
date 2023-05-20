733667071c4df0a84c2f563215758dd26bccd763
LuisaLafaurie <m1m1respald0w0@gmail.com>
2023-05-20 15:59:00 -0500

    añadiendo enlaces al navbar

--- src/components/Home/Navbar/NavBar.jsx
+++ src/components/Home/Navbar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../../assets/shared/logo.svg";
+import { NavLink } from "react-router-dom";
 import { StyledLine, StyledList, StyledNav } from "./StyleNavbar";
 
 export const NavBar = () => {
@@ -12,10 +13,10 @@ export const NavBar = () => {
 
       <StyledList>
         <ul>
-          <li>00 HOME</li>
-          <li>01 DESTINATION</li>
-          <li>02 CREW</li>
-          <li>03 TECHNOLOGY</li>
+          <li><NavLink to="/">00 HOME</NavLink></li>
+          <li><NavLink to="/destination">01 DESTINATION</NavLink></li>
+          <li><NavLink to="/crew">02 CREW</NavLink></li>
+          <li><NavLink to="/technology">03 TECHNOLOGY</NavLink></li>
         </ul>
       </StyledList>
     </StyledNav>
