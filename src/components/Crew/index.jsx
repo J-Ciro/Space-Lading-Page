dfff7c4cc79d253c2059d5be88b2751997c2d89f
lizeth montoya <lizethmontoyagarcia@gmail.com>
2023-05-20 18:39:33 -0500

    realizando la pagina del crew

--- src/components/Crew/index.jsx
+++ src/components/Crew/index.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { NavBar } from "../Home/Navbar/NavBar";
+import { crew } from "../../Data/data.js";
 
 export const Crew = () => {
+
   return (
     <>
-    <h1>Crew</h1>
+      <NavBar />
+      {crew.map((person) => (
+        <div key={person.id}>
+            <h3>{person.role}</h3>
+            <h1>{person.name}</h1>
+          <img src={person.image.png} alt={person.name} />
+          <p>{person.bio}</p>
+        </div>
+))}
     </>
   );
-};
+}
