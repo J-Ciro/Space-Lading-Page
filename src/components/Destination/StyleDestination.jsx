a1c8a717135b492a178dec7a15d0708fff692d86
Yesid Vanegas <yesidvanegas68@gmail.com>
2023-05-31 18:40:22 -0500

    Estilando informacion del planeta

--- src/components/Destination/StyleDestination.jsx
+++ src/components/Destination/StyleDestination.jsx
@@ -2,6 +2,7 @@ import { styled } from "styled-components";
 
 
 export const StylesDestination = styled.div`
+@import url('https://fonts.googleapis.com/css2?family=Barlow&family=Barlow+Condensed&display=swap');
   .titleDestination {
     position: absolute;
     position: absolute;
@@ -21,11 +22,8 @@ export const StylesDestination = styled.div`
       font-weight: 700;
       font-size: 28px;
       line-height: 34px;
-
       letter-spacing: 4.725px;
-
       color: #ffffff;
-
       mix-blend-mode: normal;
       opacity: 0.25;
     }
@@ -51,24 +49,32 @@ export const StylesDestination = styled.div`
 
   .infoDestination{
     display: flex;
-    width: 80%;
-    justify-content: space-around;
+    width: 90%;
+    
+    justify-content: space-between;
     align-items: center;
     font-family: "Barlow Condensed";
     position: absolute;
-    top: 40%;
+    top: 30%;
     color: #ffffff;
     img{
       width: 360px;
+      margin-left: 40%;
     }
 
     .informacionPlaneta{
       display: flex;
       flex-direction: column;
-      margin: 0;
-      width: 35%;
+      justify-content: space-between;
+      width: 28%;
+      margin-right: 7%;
 
+      hr{
+        margin: 8% 0% 4% 0% ;
+      }
       .navPlanetas {
+        display: flex;
+        margin-bottom: 20%;
     ul {
       display: flex;
       gap: 20px;
@@ -92,20 +98,21 @@ export const StylesDestination = styled.div`
     }
   }
   .title{
-font-family: 'Bellefair';
-font-style: normal;
-font-weight: 400;
-font-size: 100px;
-line-height: 115px;
-color: #FFFFFF;
+    font-family: 'Bellefair';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 100px;
+    line-height: 115px;
+    color: #FFFFFF;
   }
   .description{
-font-family: 'Barlow';
-font-style: normal;
-font-weight: 200;
-font-size: 18px;
-line-height: 32px;
-color: #D0D6F9;
+    font-family: 'Barlow Condensed', sans-serif;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 32px;
+    letter-spacing: 0.5px;
+    color: #D0D6F9;
 
   }
     }
